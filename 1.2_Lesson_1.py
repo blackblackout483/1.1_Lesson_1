305f6f1a051d810d4073fe0aa6e5f0b7ec3d1153
blackblackout483 <blackblackout483@gmail.com>
2024-12-16 23:35:46 +0300

    Add files via upload

---
+++ 1.2_Lesson_1.py
@@ -0,0 +1,10 @@
+home_tasks = 12
+hours = 1.5
+name_of_course = 'Python'
+time = hours / home_tasks
+
+print('Курс:', name_of_course,
+      ', всего задач:', home_tasks,
+      ', затрачено часов:', hours,
+      ', среднее время выполнения:', time, 'часа'
+      )
